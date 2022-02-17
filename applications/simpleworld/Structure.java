35c37531076c435b14fd5146e7af2edb9d376952
CAPY <alexandre.capy@gmail.com>
2022-02-17 10:58:09 +0100

    update Ville, ajout classe Structure

---
+++ applications/simpleworld/Structure.java
@@ -0,0 +1,20 @@
+package applications.simpleworld;
+
+import com.jogamp.opengl.GL2;
+
+import worlds.World;
+
+import objects.UniqueDynamicObject;
+
+
+public class Structure extends UniqueDynamicObject{
+
+	public Structure ( int __x , int __y, World __world )
+	{
+		super(__x,__y,__world);
+	}
+	
+	public void step(){}
+	
+	public void displayUniqueObject(World myWorld, GL2 gl, int offsetCA_x, int offsetCA_y, float offset, float stepX, float stepY, float lenX, float lenY, float normalizeHeight ){}
+}
--- applications/simpleworld/Ville.java
+++ applications/simpleworld/Ville.java
@@ -1,20 +1,47 @@
-package objects;
+package applications.simpleworld;
 
 import com.jogamp.opengl.GL2;
 
 import worlds.World;
 
+import java.util.ArrayList;
+
+import objects.UniqueDynamicObject;
+
+import applications.simpleworld.Structure;
 
 public class Ville extends UniqueDynamicObject{
 
-	int bois;
+	private int nourriture;	//Quantité de nourriture, nécessaire à la création d'agents
+	private int bois;		//Quantité de bois, nécessaire à la création de villages
+	private int fer;		//Quantité de fer, nécessaire à la création de soldats
+
+		
+
+	private ArrayList<Agent> agents;	//Liste de tous les agents 
+	private ArrayList<Structure> structures; //Liste de tous les villages, fermes et mines
+
 	public Ville(int __x, int __y, World __world) 
 	{
 		super(__x,__y,__world);
-			
+
+		nourriture = 10; 
+		bois = 0;
+		fer = 0;
+		
+		agents = new ArrayList<Agent>();
+		structures = new ArrayList<Structure>();
 	}
 
-	public void step(){}
+	//fonction récupère ressource
+
+	public void step(){
+	
+	//toutes les X itérations, récupère les ressources + étendre influence si possible + nourrir population
+
+	//condition si envahie -> détruire ville, le vainqueur récupère villages, fermes et mines
+		
+	}
 	
 	public void displayUniqueObject(World myWorld, GL2 gl, int offsetCA_x, int offsetCA_y, float offset, float stepX, float stepY, float lenX, float lenY, float normalizeHeight ){
 
