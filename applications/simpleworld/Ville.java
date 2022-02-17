43a7857e3b2b704b50a4aed479aa55962af7f1f4
CAPY <3700119@ppti-14-406-13.ufr-info-p6.jussieu.fr>
2022-02-17 09:30:55 +0100

    ajout de ma classe ville

---
+++ applications/simpleworld/Ville.java
@@ -0,0 +1,19 @@
+package objects;
+
+import com.jogamp.opengl.GL2;
+
+import worlds.World;
+
+public class Ville extends UniqueObject{
+
+	
+	public Ville( World __world) 
+	{
+		super(5,5,__world);
+		
+	}
+
+	public void displayUniqueObject(World myWorld, GL2 gl, int offsetCA_x, int offsetCA_y, float offset, float stepX, float stepY, float lenX, float lenY, float normalizeHeight ){
+
+	}
+}
