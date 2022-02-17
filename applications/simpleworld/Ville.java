8eaf27c76e8b26752e4783025a7e1a8b38331107
CAPY <alexandre.capy@gmail.com>
2022-02-17 10:04:59 +0100

    Update de la classe Ville

--- applications/simpleworld/Ville.java
+++ applications/simpleworld/Ville.java
@@ -4,15 +4,18 @@ import com.jogamp.opengl.GL2;
 
 import worlds.World;
 
-public class Ville extends UniqueObject{
 
-	
-	public Ville( World __world) 
+public class Ville extends UniqueDynamicObject{
+
+	int bois;
+	public Ville(int __x, int __y, World __world) 
 	{
-		super(5,5,__world);
-		
+		super(__x,__y,__world);
+			
 	}
 
+	public void step(){}
+	
 	public void displayUniqueObject(World myWorld, GL2 gl, int offsetCA_x, int offsetCA_y, float offset, float stepX, float stepY, float lenX, float lenY, float normalizeHeight ){
 
 	}
