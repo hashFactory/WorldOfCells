9854a987409d0f9e33a31ed76aee1b8a412ef197
Tristan Charpentier <tristan_charpentier@hotmail.com>
2022-02-17 11:26:17 +0100

    rajout d'util.Location

--- util/Location.java
+++ util/Location.java
@@ -1,2 +1,19 @@
-package util;public class Location {
+package util;
+
+public class Location {
+    public int x;
+    public int y;
+
+    public Location(int _x, int _y) {
+        this.x = _x;
+        this.y = _y;
+    }
+
+    public double distance(int _x, int _y) {
+        return Math.sqrt(Math.pow(_y - this.y, 2.0) + Math.pow(_x - this.x, 2.0));
+    }
+
+    public double distance(Location l) {
+        return this.distance(l.x, l.y);
+    }
 }
