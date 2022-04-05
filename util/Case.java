1c8142c70d0a087f3e1d269d09ce226186810a4b
Tristan Charpentier <tristan_charpentier@hotmail.com>
2022-04-05 14:35:35 +0200

    fix distance torique

--- util/Case.java
+++ util/Case.java
@@ -14,7 +14,15 @@ public class Case {
     }
 
     public double distance(int _x, int _y) {
-        return Math.sqrt(Math.pow(_y - this.y, 2.0) + Math.pow(_x - this.x, 2.0));
+        double lowest = dxCA * dyCA;
+
+        for (int i = -1; i <= 1; i++) {
+            for (int j = -1; j <= 1; j++) {
+                double checked = Math.sqrt(Math.pow(_x - (this.x + (i * dxCA)), 2.0) + Math.pow(_y - (this.y + (j * dyCA)), 2.0));
+                lowest = Math.min(checked, lowest);
+            }
+        }
+        return lowest;
     }
 
     public double distance(Case l) {
